2ec70dade0e894bfca5dd58405e60b51a01b5700
Harsha Vardhan <47480010+Harsha-Ambati@users.noreply.github.com>
2020-09-09 17:22:46 +0530

    Create Utf8 payload.py

---
+++ Utf8 payload.py
@@ -0,0 +1,17 @@
+print("\n\n\t\t **                                                        ** ** ** **                ")    
+print("\t\t **        **  **********  **********             ** ** ** **                ")
+print("\t\t **        **      **      **                     **       **                ") 
+print("\t\t **        **      **      **                     **       **                ")
+print("\t\t **        **      **      **********     ****    ** ** ** **                ")
+print("\t\t **        **      **      **********     ****    ** ** ** **                ")
+print("\t\t **        **      **      **                     **       **                ")
+print("\t\t **        **      **      **                     **       **                ")   
+print("\t\t ** **  ** **      **      **                     ** ** ** **                ")
+print("\t\t ** **  ** **      **      **                     ** ** ** **       -harsha            \n\n")
+
+ch = input("Give Input: ")
+print("Output:")
+for i in ch:
+        res="&#"+str(ord(i))
+        print(res,end="")
+print("\n")
